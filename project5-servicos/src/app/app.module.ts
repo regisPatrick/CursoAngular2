e7368ba7b412c05e358d187ea2e24eb16c2d45ad
regisPatrick <regis86@hotmail.com>
2020-03-12 19:06:52 -0300

    commit 12-03-20

--- project5-servicos/src/app/app.module.ts
+++ project5-servicos/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursosModule
   ],
+  providers: [LogService],
   // providers: [CursosService],
   bootstrap: [AppComponent]
 })
