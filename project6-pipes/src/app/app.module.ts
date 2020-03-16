5d718504c1796e6d7bd0569c59444f15d3fd980f
regisPatrick <regis86@hotmail.com>
2020-03-16 01:50:21 -0300

    commit 16-03-2020

--- project6-pipes/src/app/app.module.ts
+++ project6-pipes/src/app/app.module.ts
@@ -1,22 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt, 'pt');
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
+import { FiltroArrayPipe } from './filtro-array.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
-    CamelCasePipe
+    CamelCasePipe,
+    FiltroArrayPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- project6-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ project6-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -16,6 +16,15 @@ export class ExemplosPipesComponent implements OnInit {
     url: 'http://a.co/gjqjpRP'
   };
 
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: string;
+
+  addCurso(valor){
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
   constructor() { }
 
   ngOnInit() {
