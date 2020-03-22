d3c4201f281272b1db16c870b03a2c45d08dfe01
regisPatrick <regis86@hotmail.com>
2020-03-22 18:28:22 -0300

    commit 22-03-2020

--- project7-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ project7-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Routes } from '@angular/router';
+import { ActivatedRoute, Routes, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+
+import { CursosService } from '../cursos/cursos.service';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -9,10 +11,15 @@ import { Subscription } from 'rxjs';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  id: string;
+  id: number;
   inscricao: Subscription;
+  curso: any;
 
-  constructor(private route: ActivatedRoute) { 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private cursosService: CursosService  
+  ) { 
     // this.id = this.route.snapshot.params['id'];
     // console.log(this.route);
   }
@@ -21,6 +28,12 @@ export class CursoDetalheComponent implements OnInit {
     this.inscricao = this.route.params.subscribe( 
       (params: any) => {
         this.id = params['id'];
+
+        this.curso = this.cursosService.getCurso(this.id);
+
+        if (this.curso == null){
+          this.router.navigate(['/naoEncontrado']);
+        }
       } 
     );
   }
