410ed7a07728374ffaf294c66b64b713537ef6e8
regisPatrick <regis86@hotmail.com>
2020-03-23 15:08:12 -0300

    commit 23-03-2020

--- project7-rotas/src/app/cursos/cursos.component.ts
+++ project7-rotas/src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { CursosService } from './cursos.service';
 
@@ -10,11 +12,33 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
   
-  constructor(private CursosService: CursosService) { }
+  constructor(
+    private CursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.cursos = this.CursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    // this.pagina++;
+    this.router.navigate(['/cursos'], 
+      {queryParams: {'pagina': ++this.pagina}});
   }
 
 }
