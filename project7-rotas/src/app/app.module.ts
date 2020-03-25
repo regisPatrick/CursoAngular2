08d520fc8f53ac59b7f8bc54395f1f8d49ff53a3
regisPatrick <regis86@hotmail.com>
2020-03-25 01:05:50 -0300

    commit 25-03-2020

--- project7-rotas/src/app/app.module.ts
+++ project7-rotas/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing';
+// import { routing } from './app.routing';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -22,8 +22,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    routing
+    AppRoutingModule// ,
+    // routing
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
