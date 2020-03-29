c28d4db6e4adfaac7a00f03e309e6f60084dbaa0
regisPatrick <regis86@hotmail.com>
2020-03-29 18:39:32 -0300

    commit 29-03-2020

--- project7-rotas/src/app/app.routing.module.ts
+++ project7-rotas/src/app/app.routing.module.ts
@@ -7,15 +7,24 @@ import { LoginComponent } from './login/login.component';
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 // import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
-    {path: 'cursos', loadChildren: './cursos/cursos.module#CursosModule'},
-    {path: 'alunos', loadChildren: './alunos/alunos.module#AlunosModule'},
+    {path: 'cursos',
+        loadChildren: './cursos/cursos.module#CursosModule',
+        canActivate: [AuthGuard]
+    },
+    {path: 'alunos', 
+        loadChildren: './alunos/alunos.module#AlunosModule',
+        canActivate: [AuthGuard]
+    },
     // { path: 'cursos', component: CursosComponent },
     // { path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent },
     // { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent, 
+        canActivate: [AuthGuard]
+    }
 ];
 
 @NgModule({
