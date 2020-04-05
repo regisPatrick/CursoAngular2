a7aadd71d74665ddf34f35360615ca45f1f9eda7
regisPatrick <regis86@hotmail.com>
2020-04-04 22:54:18 -0300

    commit 04-04-2020

--- project7-rotas/src/app/app.routing.module.ts
+++ project7-rotas/src/app/app.routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 // import { CursosComponent } from './cursos/cursos.component';
 // import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 // import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
@@ -28,9 +29,11 @@ const appRoutes: Routes = [
     // { path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent },
     // { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
-    { path: '', component: HomeComponent, 
+    { path: 'home', component: HomeComponent, 
         canActivate: [AuthGuard]
-    }
+    },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', component: PaginaNaoEncontradaComponent /* , canActivate: [AuthGuard] */ }
 ];
 
 @NgModule({
