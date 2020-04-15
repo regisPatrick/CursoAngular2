dac6c2465f480fd8ede68723b99fc5355ede9480
regisPatrick <regis86@hotmail.com>
2020-04-15 02:52:14 -0300

    commit 15-04-2020

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -18,6 +18,11 @@ export class TemplateFormComponent implements OnInit {
     console.log(form);
 
     // console.log(this.usuario);
+
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value))
+      // .map(res => res)
+      .subscribe(dados => console.log(dados));    
+
   }
 
   constructor(private http: HttpClient) { }
