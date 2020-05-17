72de4da6bc48874ac888128b2b4b559f4d9309e6
regisPatrick <regis86@hotmail.com>
2020-05-16 23:34:00 -0300

    commit 16-05-2020 2

---
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -0,0 +1,23 @@
+import { tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Curso } from './curso';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = 'http://localhost:3000/cursos';
+
+  constructor(private http: HttpClient) { }
+
+  list(){
+    return this.http.get<Curso[]>(this.API)
+    .pipe(
+      tap(console.log)
+    );
+  }
+
+}
