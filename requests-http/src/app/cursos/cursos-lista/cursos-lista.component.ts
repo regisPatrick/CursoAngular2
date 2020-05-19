8d9f96a1044e8935105c3bc5e7ef7dfa99a8f351
regisPatrick <regis86@hotmail.com>
2020-05-19 16:06:02 -0300

    commit 19-05-2020 2

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,8 +1,9 @@
-import { Observable } from 'rxjs';
+import { Observable, empty, Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -15,15 +16,39 @@ export class CursosListaComponent implements OnInit {
   // cursos: Curso[];
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
   ngOnInit() {
     // this.service.list()
     // .subscribe(dados => this.cursos = dados);
-
-    this.cursos$ = this.service.list();
-
+    this.onRefresh();
   }
+
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      // map(),
+      // tap(),
+      // switchMap(),
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
+    this.service.list()
+    .pipe(
+      catchError(error => empty())
+    )
+    .subscribe(
+      dados => {
+        console.log(dados);
+      }// ,
+      // error => console.error(error),
+      // () => console.log('Observable completo!')
+    );
+  }  
 
 }
