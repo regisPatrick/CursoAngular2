755cdf4b808d3cea0e62cdffd1f3bacdef6113cc
regisPatrick <regis86@hotmail.com>
2020-05-19 00:38:31 -0300

    commit 19-05-2020

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { UnsubscribeRxjsModule } from './unsubscribe-rxjs/unsubscribe-rxjs.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnsubscribeRxjsComponent } from './unsubscribe-rxjs/unsubscribe-rxjs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnsubscribeRxjsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    UnsubscribeRxjsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
