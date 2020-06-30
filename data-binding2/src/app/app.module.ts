f71322b6f0452481e638fb9b7eecad0e168f22c8
regisPatrick <regis86@hotmail.com>
2020-06-30 19:25:14 -0300

    commit 30-06-2020

--- data-binding2/src/app/app.module.ts
+++ data-binding2/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
