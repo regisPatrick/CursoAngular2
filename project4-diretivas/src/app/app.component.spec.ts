c63e9b6bf9262ee635e74dde6a71f39e0e4b9fee
regisPatrick <regis86@hotmail.com>
2020-07-08 16:10:52 -0300

    commit 08-07-2020

--- project4-diretivas/src/app/app.component.spec.ts
+++ project4-diretivas/src/app/app.component.spec.ts
@@ -20,16 +20,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project4-diretivas'`, () => {
+  it(`should have as title 'app works!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project4-diretivas');
+    expect(app.title).toEqual('app works!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project4-diretivas app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    // expect(compiled.querySelector('.content span').textContent).toContain('project4-diretivas app is running!');
   });
 });
