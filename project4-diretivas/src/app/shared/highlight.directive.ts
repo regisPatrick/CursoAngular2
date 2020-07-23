c51dee2a22a5f632c190f0009a807bd3c897ed9c
regisPatrick <regis86@hotmail.com>
2020-07-22 23:59:22 -0300

    commit 22-07-2020 2

--- project4-diretivas/src/app/shared/highlight.directive.ts
+++ project4-diretivas/src/app/shared/highlight.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
 
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  @Input() defaultColor: string = 'white';
+
+  @Input() highlightColor: string = 'yellow';
+
+  @HostListener('mouseenter') onMouseOver(){
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.backgroundColor = this.defaultColor;
+  }
+
   constructor() { }
+
+  ngOnInit(){
+    this.backgroundColor = this.defaultColor;
+  }
 
 }
