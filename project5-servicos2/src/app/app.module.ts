f280f0333d7e18735a65f9f3168d3c0fbbd2d63e
regisPatrick <regis86@hotmail.com>
2020-07-27 22:44:20 -0300

    commit 27-07-2020

--- project5-servicos2/src/app/app.module.ts
+++ project5-servicos2/src/app/app.module.ts
@@ -3,19 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [CursosService],
+  // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
