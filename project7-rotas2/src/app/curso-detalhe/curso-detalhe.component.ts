5541cc1c02fb2c9c3a992cece9953d13988f70f4
regisPatrick <regis86@hotmail.com>
2020-08-10 19:46:49 -0300

    commit 10-08-2020

--- project7-rotas2/src/app/curso-detalhe/curso-detalhe.component.ts
+++ project7-rotas2/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent implements OnInit {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
 
   id: string;
+  inscricao: Subscription;
 
   constructor(private route: ActivatedRoute) {
+    // this.id = this.route.snapshot.params['id'];
     // console.log(this.route);
-    this.id = this.route.snapshot.params['id'];
   }
 
   ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
