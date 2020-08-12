dd0a08f4dda38b53004d5cdd7c2a859832bf21dc
regisPatrick <regis86@hotmail.com>
2020-08-12 14:54:32 -0300

    commit 12-08-2020

--- project7-rotas2/src/app/cursos/cursos.component.ts
+++ project7-rotas2/src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { CursosService } from './cursos.service';
 
@@ -7,14 +9,37 @@ import { CursosService } from './cursos.service';
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, OnDestroy {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    // this.pagina++;
+    this.router.navigate(['/cursos'],
+      // tslint:disable-next-line: object-literal-key-quotes
+      {queryParams: {'pagina': ++this.pagina}});
   }
 
 }
