562e254148a2b9a80a7e67c8dd6358216fafdbed
regisPatrick <regis86@hotmail.com>
2020-08-15 15:50:52 -0300

    commit 15-08-2020

--- project7-rotas2/src/app/app-routing.module.ts
+++ project7-rotas2/src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+// import { CursosComponent } from './cursos/cursos.component';
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const routes: Routes = [
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent },
+  // { path: 'cursos', component: CursosComponent },
+  // { path: 'curso/:id', component: CursoDetalheComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
+  // { path: 'naoEncontrado', component: CursoNaoEncontradoComponent},
   { path: '', component: HomeComponent }
 ];
 
