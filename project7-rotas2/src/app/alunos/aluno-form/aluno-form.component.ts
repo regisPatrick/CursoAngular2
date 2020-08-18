059ba18b6232436c6dd0906f5bbdb84db57f8720
regisPatrick <regis86@hotmail.com>
2020-08-17 23:57:02 -0300

    commit 17-08-2020 4

--- project7-rotas2/src/app/alunos/aluno-form/aluno-form.component.ts
+++ project7-rotas2/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { AlunosService } from '../alunos.service';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit {
+export class AlunoFormComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  aluno: any = {};
+  inscricao: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private alunosService: AlunosService
+  ) { }
 
   ngOnInit(): void {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        // tslint:disable-next-line: prefer-const
+        let id = params['id'];
+
+        this.aluno = this.alunosService.getAluno(id);
+
+        if (this.aluno === null){
+          this.aluno = {};
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
   }
 
 }
