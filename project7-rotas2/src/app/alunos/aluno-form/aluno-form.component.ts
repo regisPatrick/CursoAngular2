2b81dd3b8e8899201072450fb8b1d65bfeb53328
regisPatrick <regis86@hotmail.com>
2020-08-23 15:05:37 -0300

    commit 23-08-2020

--- project7-rotas2/src/app/alunos/aluno-form/aluno-form.component.ts
+++ project7-rotas2/src/app/alunos/aluno-form/aluno-form.component.ts
@@ -3,13 +3,14 @@ import { Subscription } from 'rxjs';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { AlunosService } from '../alunos.service';
+import { IFormCanDeactivate } from '../../guards/iform-candeactivate';
 
 @Component({
   selector: 'app-aluno-form',
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit, OnDestroy {
+export class AlunoFormComponent implements OnInit, OnDestroy, IFormCanDeactivate {
 
   aluno: any = {};
   inscricao: Subscription;
--- project7-rotas2/src/app/guards/alunos-deactivate.guard.ts
+++ project7-rotas2/src/app/guards/alunos-deactivate.guard.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { AlunoFormComponent } from './../alunos/aluno-form/aluno-form.component';
+import { IFormCanDeactivate } from './iform-candeactivate';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AlunosDeactivateGuard implements CanDeactivate<AlunoFormComponent> {
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> {
 
     canDeactivate(
-      component: AlunoFormComponent,
+      component: IFormCanDeactivate,
       route: ActivatedRouteSnapshot,
       state: RouterStateSnapshot
     ): Observable<boolean>|Promise<boolean>|boolean {
