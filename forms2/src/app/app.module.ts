b9f7dd63d5e897676e742ae5b0a1d70400500b57
regisPatrick <regis86@hotmail.com>
2020-08-30 14:18:38 -0300

    commit 30-08-2020

--- forms2/src/app/app.module.ts
+++ forms2/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -14,7 +15,8 @@ import { DataFormComponent } from './data-form/data-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
