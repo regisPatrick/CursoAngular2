abcdc53f47c42be6df9c42ca963cff74c4e46ff0
regisPatrick <regis86@hotmail.com>
2020-09-03 22:34:25 -0300

    commit 03-09-2020

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@Angular/core';
-
-@Component({
-    selector: 'meu-primeiro-component',
-    template: `
-        <p>Meu Primeiro component com Angular!</p>
-    `
-})
-export class MeuPrimeiroComponent { }
