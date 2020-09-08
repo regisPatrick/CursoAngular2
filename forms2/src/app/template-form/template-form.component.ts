685b04dd488e1de86ddab298c7e0285dc31c3746
regisPatrick <regis86@hotmail.com>
2020-09-08 19:29:51 -0300

    commit 08-09-2020

--- forms2/src/app/template-form/template-form.component.ts
+++ forms2/src/app/template-form/template-form.component.ts
@@ -23,4 +23,15 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  verificaValidTouched(campo){
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo){
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    };
+  }
+
 }
