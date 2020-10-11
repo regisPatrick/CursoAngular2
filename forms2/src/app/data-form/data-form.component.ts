6eb027720d4d24f043dd275416697221e79036a9
regisPatrick <regis86@hotmail.com>
2020-10-11 17:48:27 -0300

    commit 11-10-2020

--- forms2/src/app/data-form/data-form.component.ts
+++ forms2/src/app/data-form/data-form.component.ts
@@ -69,7 +69,8 @@ export class DataFormComponent implements OnInit {
 
       cargo: [null],
       tecnologias: [null],
-      newsletter: ['s']
+      newsletter: ['s'],
+      termos: [null, Validators.pattern('true')]
     });
 
     // tslint:disable-next-line: max-line-length
