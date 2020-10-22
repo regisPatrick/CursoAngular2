6ccefb38124765a2c69975d00e5d2ee141d5eeed
regisPatrick <regis86@hotmail.com>
2020-10-21 23:51:07 -0300

    commit 21-10-2020 2

--- forms2/src/app/shared/form-validations.ts
+++ forms2/src/app/shared/form-validations.ts
@@ -21,6 +21,11 @@ export class FormValidations {
   }
 
   static cepValidator(control: FormControl){
+    const cep = control.value;
+    if (cep && cep !== ''){
+      const validacep = /^[0-9]{8}$/;
+      return validacep.test(cep) ? null : { cepInvalido: true };
+    }
     return null;
   }
 
