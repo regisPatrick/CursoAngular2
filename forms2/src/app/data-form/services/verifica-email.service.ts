b22b1206f05417d8c3d5b9829eb83e4fad8a66f7
regisPatrick <regis86@hotmail.com>
2020-10-30 23:54:14 -0300

    commit 30-10-2020

--- forms2/src/app/data-form/services/verifica-email.service.ts
+++ forms2/src/app/data-form/services/verifica-email.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,10 @@ export class VerificaEmailService {
 constructor(private http: HttpClient) { }
 
 verificarEmail(email: string) {
-  return this.http.get('assets/dados/verificarEmail.json');
+  return this.http.get('assets/dados/verificarEmail.json')
+    .pipe(
+      map((dados: { emails: any[]}) => dados.emails);
+    )
 }
 
 }
