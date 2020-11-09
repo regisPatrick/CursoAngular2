0563d7c95a51e91d4525ec8cdfe28e5c1d8d99c2
regisPatrick <regis86@hotmail.com>
2020-11-08 23:59:19 -0300

    commit 08-11-2020 2

--- forms2/src/app/shared/shared.module.ts
+++ forms2/src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { ErrorMsgComponent } from './error-msg/error-msg.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -8,7 +9,8 @@ import { CampoControlErroComponent } from './campo-control-erro/campo-control-er
 @NgModule({
   declarations: [
     FormDebugComponent,
-    CampoControlErroComponent
+    CampoControlErroComponent,
+    ErrorMsgComponent
   ],
   imports: [
     CommonModule,
