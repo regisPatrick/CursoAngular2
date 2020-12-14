a9f9b148c66a1d49770da6fb1b1971bc44382941
regisPatrick <regis86@hotmail.com>
2020-12-14 00:32:16 -0300

    commit 14-12-2020

--- forms2/src/app/shared/input-field/input-field.component.ts
+++ forms2/src/app/shared/input-field/input-field.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
   styleUrls: ['./input-field.component.css']
 })
-export class InputFieldComponent implements OnInit {
+export class InputFieldComponent implements OnInit, ControlValueAccessor {
 
   @Input() classeCss;
   @Input() id: string;
@@ -16,6 +17,19 @@ export class InputFieldComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  writeValue(obj: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
   }
 
 }
