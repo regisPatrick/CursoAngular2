6a8e194cc9e2e38293a157e173ab735406e4941c
regisPatrick <regis86@hotmail.com>
2021-03-11 00:38:05 -0300

    commit 11-03-2021

---
+++ cliente-pesquisa-google/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BuscaGoogleComponent } from './busca-google/busca-google.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BuscaGoogleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cliente-pesquisa-google/src/app/busca-google/busca-google.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-busca-google',
+  templateUrl: './busca-google.component.html',
+  styleUrls: ['./busca-google.component.css']
+})
+export class BuscaGoogleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
